c8a1aadd6d37d83c1e7069d6fe5ca49dad64f1a4
Mox Alehin <moxalehin@gmail.com>
2025-03-14 13:42:14 +0300

    Apple local settings on Enter press

--- src/modals/local-settings-modal.ts
+++ src/modals/local-settings-modal.ts
@@ -6,33 +6,33 @@ type EditableSettings = "width" | "slots" | "ratio" | "from" | "where" | "image"
 const AVAILABLE_SETTINGS: EditableSettings[] = ["width", "slots", "ratio", "from", "where", "image"];
 
 export class LocalSettingsModal extends Modal {
-	private settings: TierListSettings;
-	private onSave: (updatedSettings: Partial<TierListSettings>) => void;
+    private settings: TierListSettings;
+    private onSave: (updatedSettings: Partial<TierListSettings>) => void;
 
-	constructor(app: App, settings: TierListSettings, onSave: (updatedSettings: Partial<TierListSettings>) => void) {
-		super(app);
-		this.settings = settings;
-		this.onSave = onSave;
-	}
+    constructor(app: App, settings: TierListSettings, onSave: (updatedSettings: Partial<TierListSettings>) => void) {
+        super(app);
+        this.settings = settings;
+        this.onSave = onSave;
+    }
 
     init(settings: TierListSettings) {
         const { contentEl } = this;
-		contentEl.empty();
+        contentEl.empty();
 
-		this.setTitle("Local Settings");
+        this.setTitle("Local Settings");
 
-		const updatedSettings: Partial<TierListSettings> = {};
+        const updatedSettings: Partial<TierListSettings> = {};
 
-		AVAILABLE_SETTINGS.forEach((key) => {
+        AVAILABLE_SETTINGS.forEach((key) => {
             updatedSettings[key] = settings[key] as any;
             new Setting(contentEl)
                 .setName(key.charAt(0).toUpperCase() + key.slice(1))
                 .addText((text) => {
                     const initialValue = settings[key] as string | number;
-        
+
                     text.setValue(String(initialValue)).onChange((value) => {
                         const defaultValue = DEFAULT_SETTINGS[key];
-        
+
                         if (typeof defaultValue === "number") {
                             (updatedSettings as Record<string, number | string>)[key] = Number(value) || 0;
                         } else {
@@ -42,19 +42,27 @@ export class LocalSettingsModal extends Modal {
                 });
         });
 
-		new Setting(contentEl)
+        new Setting(contentEl)
             .addButton((btn) => btn.setIcon("undo-2").setCta().onClick(() => {
                 this.init(DEFAULT_SETTINGS);
             }))
-			.addButton((btn) =>
-				btn.setIcon("check").setCta().onClick(() => {
-					this.onSave(updatedSettings);
-					this.close();
-				})
-			);
+            .addButton((btn) =>
+                btn.setIcon("check").setCta().onClick(() => {
+                    onSubmitHandler();
+                })
+            );
+        this.scope.register([], "Enter", (evt) => {
+            evt.preventDefault();
+            onSubmitHandler();
+        });
+
+        const onSubmitHandler = () => {
+            this.onSave(updatedSettings);
+            this.close();
+        };
     }
 
-	onOpen() {
-		this.init(this.settings);
-	}
+    onOpen() {
+        this.init(this.settings);
+    }
 }
